d6bd443971888993535aeaf5d617490b480d0611
Kaustubh1532 <68048023+Kaustubh1532@users.noreply.github.com>
2020-07-09 12:07:52 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+function toggle(){
+
+  var navbar=document.getElementById("nav-links")
+
+  if (navbar.style.display=="block"){
+    navbar.style.display="none";
+  }
+  else{
+    navbar.style.display="block";
+  }
+}
+
